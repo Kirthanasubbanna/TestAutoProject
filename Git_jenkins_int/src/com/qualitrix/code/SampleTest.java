28513e2292001118911c225a4c26c7a1dd4d0328
Kirthanasubbanna <55536817+Kirthanasubbanna@users.noreply.github.com>
2019-09-19 16:27:48 +0530

    Update SampleTest.java

--- Git_jenkins_int/src/com/qualitrix/code/SampleTest.java
+++ Git_jenkins_int/src/com/qualitrix/code/SampleTest.java
@@ -5,7 +5,7 @@ public class SampleTest
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello welcome to Automation ");
+		System.out.println("Hello welcome to Automation test world ");
 	}
 	
 	
