564a9d6b1b9199702e8d1b997f28b18a8b87d9d2
qualitrix-kirthana <qualitrix-kirthana@Qualitrix-Kirthana>
2019-09-19 16:34:17 +0530

    2nd commit

--- Git_jenkins_int/src/com/qualitrix/code/SampleTest.java
+++ Git_jenkins_int/src/com/qualitrix/code/SampleTest.java
@@ -6,6 +6,7 @@ public class SampleTest
 	public static void main(String[] args)
 	{
 		System.out.println("Hello welcome to Automation test world ");
+		System.out.println("Testing from Qualitrix");
 	}
 	
 	
